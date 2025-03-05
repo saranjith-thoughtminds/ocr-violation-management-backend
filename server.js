26e636a32b508e5ed3225f1492049546ef90c64d
Saranjith <saranvlmna@gmail.com>
2025-03-05 12:54:18 +0530

    Update server port environment variable to APP_PORT

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import ocrRouter from "./src/ocr/router.js";
 import violationRouter from "./src/violation/router.js";
 
 const app = express();
-const port = process.env.SERVICE_PORT;
+const port = process.env.APP_PORT;
 await cosmosConnection();
 
 app.use(json());
